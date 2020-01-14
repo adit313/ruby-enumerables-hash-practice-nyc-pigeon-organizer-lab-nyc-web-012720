7155303a80c8f945bb003294b57f1054fad7ab72
Adit P Patel <app2116@gmail.com>
2020-01-14 22:44:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   result = {}
   data.each{|k,v|
     v.each{|internal_key, value| 
-      result[value][k] << internal_key.to_s
+      result[value.][k] << internal_key.to_s
   }
  }
     return result
